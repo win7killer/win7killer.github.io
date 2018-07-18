4bf16137e3bbd38e746d84e772754d338924d560
win7killer <win7killer@163.com>
2018-07-18 17:55:36 +0800

    go

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
     dev: {
 
         // Paths
-        assetsSubDirectory: 'dist/static',
+        assetsSubDirectory: 'static',
         assetsPublicPath: '/',
         proxyTable: {},
 
@@ -50,7 +50,7 @@ module.exports = {
         // Paths
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/',
+        assetsPublicPath: '/dist',
 
         /**
          * Source Maps
