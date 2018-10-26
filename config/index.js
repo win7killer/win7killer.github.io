bcde2ccbbe9a774cbc9168675ffe56127a6b7630
win7killer <win7killer@163.com>
2018-10-26 19:24:52 +0800

    update1

--- config/index.js
+++ config/index.js
@@ -48,9 +48,9 @@ module.exports = {
         index: path.resolve(__dirname, '../index.html'),
 
         // Paths
-        assetsRoot: path.resolve(__dirname, '../dist'),
+        assetsRoot: path.resolve(__dirname, '../dist/'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/dist',
+        assetsPublicPath: '/dist/',
 
         /**
          * Source Maps
